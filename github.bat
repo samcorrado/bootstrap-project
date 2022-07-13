1bac08f94609ccdd9f8af8f513d220620af1de13
sam corrado <samcorrado08@gmail.com>
2022-07-13 15:28:48 -0400

    changed name and email value

--- github.bat
+++ github.bat
@@ -1,6 +1,7 @@
 :: windows batch file to set github username & email
-git config --global user.name "your name"
-git config --global user.email youremail@address.com
+git config --global user.name "sam corrado"
+git config --global user.email samcorrado08@gmail.com
 git config --global core.editor "'C:/Program Files/Notepad++/notepad++.exe' -multiInst -notabbar -nosession -noPlugin"
 :: just for funsies, to list all the things set in git config
 git config --list
+::changed name and email value
